f1b3ed0a1b21301d27e1ba8e7dd4107ffa1032b1
Zhengyi Luo <zhengyil@nvidia.com>
2022-10-28 17:27:06 -0400

    new data download script

--- downlaod_data.sh
+++ downlaod_data.sh
@@ -1,6 +1,7 @@
 mkdir sample_data
 mkdir sample_data/meta
 mkdir sample_data/features
+mkdir -p results/all/statear/kin_poly/models_policy/ results/motion_im/uhc/models
 gdown https://drive.google.com/uc?id=1W79g1It7VG6BWHbBZolbIkcWZsGlOCm9 -O  sample_data/
 gdown https://drive.google.com/uc?id=1oFQ7aOxpGXmnizs76viV-z5-a32u95HB -O  sample_data/
 gdown https://drive.google.com/uc?id=1GWndIhZZ91s6blqWccUzbPhHqrAeg7u1 -O  sample_data/features/ # Mocap annotations
@@ -9,3 +10,5 @@ gdown https://drive.google.com/uc?id=1qqKkWyIyT4rZyY91beRibjKqEB7iePXU -O  sampl
 gdown https://drive.google.com/uc?id=1nkHGAnaMr-kog4XhIyLUcEEpxn2A1C4L -O  sample_data/features/ # real_world annotations
 gdown https://drive.google.com/uc?id=1VxPliVe0dxAY1qGllnYxjbQsGlc4XYV8 -O  sample_data/meta/ # real world meta data
 gdown https://drive.google.com/uc?id=1ZAHbM3iYe1Wq0ShTGeDpFhaZQAqhxHQb -O  sample_data/ # amass_occlusion
+gdown https://drive.google.com/uc?id=1Hw2E8H0hHx9JwQXNsmWM0OjE1XTgFkmd -O results/motion_im/uhc/models/ # uhc trained model 
+gdown https://drive.google.com/uc?id=1oQZzWVfWPrGzX0XyB0k4h7z6WLtSEsjX -O results/all/statear/kin_poly/models_policy/ # kin_poly trained model
